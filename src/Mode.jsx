a0cb619d5d5058ee1c2a9a4e58bfcd3523276720
Johan Swan <jswannieman@gmail.com>
2023-12-30 10:13:08 +1300

    Commit Mode JSX Component

--- src/Mode.jsx
+++ src/Mode.jsx
@@ -1,7 +1,18 @@
 import { useState } from "react";
+import AnotherMode from "./AnotherMode";
 
 const Mode = () => {
   const [darkMode, setDarkMode] = useState(false);
+  const [extraModeIsShown, setExtraModeIsShown] = useState(false);
+
+  const handleExtraMode = () => {
+    setExtraModeIsShown(true);
+  };
+
+  const handleNormalMode = () => {
+    setExtraModeIsShown(false);
+  };
+
   return (
     <div className={`page${darkMode && "dark"}`}>
       <div className="buttons">
@@ -11,6 +22,13 @@ const Mode = () => {
         <button className="button-light" onClick={() => setDarkMode(false)}>
           Light Mode
         </button>
+        <button className="button-extra" onClick={handleExtraMode}>
+          Extra Modes
+        </button>
+        <button className="button-extra" onClick={handleNormalMode}>
+          Normal Modes
+        </button>
+        {extraModeIsShown && <AnotherMode />}
       </div>
     </div>
   );
